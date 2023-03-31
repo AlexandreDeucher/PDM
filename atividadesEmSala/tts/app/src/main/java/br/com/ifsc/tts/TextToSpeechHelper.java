a8d648abb199e6d970e90bc6eb33e2f4bb43cea6
alexandre deucher <alexandredeucher@gmail.com>
2023-03-31 13:52:26 -0300

    tts

---
+++ atividadesEmSala/tts/app/src/main/java/br/com/ifsc/tts/TextToSpeechHelper.java
@@ -0,0 +1,32 @@
+package br.com.ifsc.tts;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+
+import java.util.Locale;
+
+public class TextToSpeechHelper implements TextToSpeech.OnInitListener {
+    private TextToSpeech tts;
+    private boolean isReady = false;
+
+    public TextToSpeechHelper(Context context) {
+        tts = new TextToSpeech(context, this);
+    }
+
+    public void speak(String text) {
+        if (isReady) {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            tts.setLanguage(Locale.getDefault());
+            isReady = true;
+        }else{
+            Log.d("tts", "falha na inicialização do TTS ");
+        }
+    }
+}
